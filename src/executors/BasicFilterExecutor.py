554e9479368cbaf32a2df060826f37f8d805c58c
Ecrin <ecrinyetim138@gmail.com>
2025-11-16 20:52:12 +0300

    Executor Update

--- src/executors/BasicFilterExecutor.py
+++ src/executors/BasicFilterExecutor.py
@@ -13,8 +13,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 from sdks.novavision.src.media.image import Image
 from sdks.novavision.src.base.component import Component
 from sdks.novavision.src.helper.executor import Executor
-from components.Package.src.utils.response import build_response_basic_filter
-from components.Package.src.models.PackageModel import PackageModel
+from components.ImageFilterDemo.src.utils.response import build_response_basic_filter
+from components.ImageFilterDemo.src.models.PackageModel import PackageModel
 
 class BasicFilterExecutor(Component):
     def __init__(self, request, bootstrap):
