58242f5e021de205abc2e2b91f480502cca4def8
Ecrin <ecrinyetim138@gmail.com>
2025-11-22 16:06:54 +0300

    CompareAndDescribe Executor Update

--- src/executors/CompareAndDescribe.py
+++ src/executors/CompareAndDescribe.py
@@ -5,7 +5,6 @@
 import os
 import cv2
 import sys
-from skimage.metrics import structural_similarity as ssim
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
 
@@ -29,18 +28,27 @@ class CompareAndDescribe(Component):
         return {}
 
     def compare(self, img1, img2):
+        # Boyut eşitleme
         if img1.shape != img2.shape:
             img2 = cv2.resize(img2, (img1.shape[1], img1.shape[0]))
 
-        gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-        gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        # Piksel farkı
+        diff = img1.astype("float") - img2.astype("float")
 
-        score, diff = ssim(gray1, gray2, full=True)
-        similarity_percentage = score * 100
+        # Benzerlik yüzdesi
+        mean_diff = abs(diff).mean()
+        max_val = 255.0
+        similarity_percentage = max(0.0, min(100.0, (1 - mean_diff / max_val) * 100))
 
-        diff = (diff * 255).astype("uint8")
-        diff_color = cv2.applyColorMap(255 - diff, cv2.COLORMAP_JET)
+        # Diff görseli oluştur (renklendirilmiş)
+        diff_vis = abs(diff)
+        if diff_vis.max() > 0:
+            diff_vis = (diff_vis / diff_vis.max() * 255.0).astype("uint8")
+        else:
+            diff_vis = diff_vis.astype("uint8")
+        diff_color = cv2.applyColorMap(255 - diff_vis, cv2.COLORMAP_JET)
 
+        # Text output
         if self.outputFormat == "Percentage":
             text = f"{similarity_percentage:.2f}% benzerlik"
         else:
@@ -57,11 +65,14 @@ class CompareAndDescribe(Component):
         return text, diff_color
 
     def run(self):
+        # Görselleri al
         img1 = Image.get_frame(img=self.image, redis_db=self.redis_db)
         img2 = Image.get_frame(img=self.image2, redis_db=self.redis_db)
 
+        # Karşılaştır
         compare_text, diff_image = self.compare(img1.value, img2.value)
 
+        # Diff görselini Redis’e kaydet
         diff_img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         diff_img.value = diff_image
 
@@ -70,7 +81,6 @@ class CompareAndDescribe(Component):
             package_uID=self.uID,
             redis_db=self.redis_db
         )
-
         self.compareResult = compare_text
         packageModel = build_response_compare_and_describe(context=self)
         return packageModel
