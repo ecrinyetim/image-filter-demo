3535cd2f0d14b68f6c2682560941254d26c641fc
Ecrin <ecrinyetim138@gmail.com>
2025-12-12 20:27:47 +0300

    CompareAndDescribe Executor Update

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,12 +1,12 @@
 
 from sdks.novavision.src.helper.package import PackageHelper
-from components.ImageFilterDemo.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor , OutputImage, OutputText,BasicFilterResponse,BasicFilterOutputs,BasicFilter,CompareAndDescribeResponse,CompareAndDescribeOutputs,CompareAndDescribe
+from components.ImageFilterDemo.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor , OutputImage, OutputText, BasicFilterResponse, BasicFilterOutputs, BasicFilter, CompareAndDescribeResponse, CompareAndDescribeOutputs, CompareAndDescribe
 
 def build_response_basic_filter(context):
     outputImage = OutputImage(value=context.image)
-    BasicFilterOutputs = BasicFilterOutputs(outputImage=outputImage)
-    BasicFilterResponse = BasicFilterResponse(outputs=BasicFilterOutputs)
-    BasicFilter= BasicFilter(value=BasicFilterResponse)
+    basicFilterOutputs = BasicFilterOutputs(outputImage=outputImage)
+    basicFilterResponse = BasicFilterResponse(outputs=BasicFilterOutputs)
+    basicFilter= BasicFilter(value=BasicFilterResponse)
     executor = ConfigExecutor(value=BasicFilter)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
@@ -16,9 +16,9 @@ def build_response_basic_filter(context):
 def build_response_compare_and_describe(context):
     outputImage = OutputImage(value=context.image)
     outputText =OutputText(value=context.text)
-    CompareAndDescribOutputs = CompareAndDescribeOutputs(outputImage=outputImage,outputText=outputText)
-    CompareAndDescribeResponse = CompareAndDescribeResponse(outputs=CompareAndDescribeOutputs)
-    CompareAndDescribe= CompareAndDescribe(value=CompareAndDescribeResponse)
+    compareAndDescribOutputs = CompareAndDescribeOutputs(outputImage=outputImage,outputText=outputText)
+    compareAndDescribeResponse = CompareAndDescribeResponse(outputs=CompareAndDescribeOutputs)
+    compareAndDescribe= CompareAndDescribe(value=CompareAndDescribeResponse)
     executor = ConfigExecutor(value=CompareAndDescribe)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
